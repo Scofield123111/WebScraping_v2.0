6b066577b0e5b7dc20d9a4df59a856d68860c0d7
s1mple <798342418@qq.com>
2017-10-14 21:59:18 +0800

    java webscraping v2.0 by Zhangzhen

--- src/main/WebScraping.java
+++ src/main/WebScraping.java
@@ -85,7 +85,7 @@ class WebScraping {
             in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             boolean isBelongToList = false;//是否是HTML页面list标签内的内容
             int dtCounter = 0;//dt标签计数器
-            int notRequiredDtCounter = 0;//不需要的dt标签计数器
+            int unNeededDtCounter = 0;//不需要的dt标签计数器
             String urlMatchString = "<a href=\"/" + novelNumbering + "/";
 
             String line;
@@ -98,11 +98,11 @@ class WebScraping {
                 if (isBelongToList && line.contains("<dt>")) {
                     dtCounter++;
                     if (line.contains("最新章节") || line.contains("作品相关")) {
-                        notRequiredDtCounter++;
+                        unNeededDtCounter++;
                     }
                 }
 
-                if (isBelongToList && dtCounter > notRequiredDtCounter && line.contains(urlMatchString)) { //因为第一个<dt>标签是最新章节的段落，所以从第二个开始
+                if (isBelongToList && dtCounter > unNeededDtCounter && line.contains(urlMatchString)) { //因为第一个<dt>标签是最新章节的段落，所以从第二个开始
                     chapterCounter++;
                     String chapterUrl = urlPrefix + line.substring(line.indexOf(novelNumbering) - 1, line.indexOf("\">"));
                     ncQueue.add(new NovelChapter(chapterCounter, chapterUrl));
